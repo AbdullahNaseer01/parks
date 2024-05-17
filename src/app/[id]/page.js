51b7cfc7685243e1589133a43c70cd3e7f14b5be
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-05-17 09:12:17 +0500

    feat: details page designing (continue)

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchParkDetails } from "../redux/slices/parksSlice";
 import AnimatedCards from "../components/AnimatedCards/AnimatedCards";
 import LocationDetails from "../components/Location/LocationDetails";
+import DetailsHeroSection from "../components/detailsHeroSection/DetailsHeroSection";
 
 const Page = ({ params }) => {
   const dispatch = useDispatch();
@@ -23,13 +24,13 @@ const Page = ({ params }) => {
 
   return (
     <div>
-      <h1>Park Details</h1>
       {parkDetails ? (
         <div>
+          <DetailsHeroSection images={parkDetails?.images} interval={5000} />
           <h2>{parkDetails.fullName}</h2>
           <p>Description: {parkDetails.description}</p>
-          <AnimatedCards images={parkDetails?.images}/>
-          <LocationDetails/>
+          <AnimatedCards images={parkDetails?.images} />
+          <LocationDetails />
         </div>
       ) : (
         <p>Loading...</p>
---
+++ src/app/components/detailsHeroSection/DetailsHeroSection.jsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+const DetailsHeroSection = ({ images, interval = 3000 }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, interval);
+
+    return () => clearInterval(timer);
+  }, [images.length, interval]);
+
+  return (
+    <div className="relative h-screen w-full">
+      <img
+        key={currentIndex}
+        src={images[currentIndex].url}
+        alt={images[currentIndex].altText}
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-50" />
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
+        <h1 className="text-4xl text-white font-bold">{images[currentIndex].title}</h1>
+        <p className="text-xl text-white mt-4">{images[currentIndex].caption}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DetailsHeroSection;
