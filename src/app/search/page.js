c9559f0e115eb12dbfec0c5cd02528fb1c3bc60d
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-05-19 22:30:44 +0500

    fix: deployment error

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Navbar from "../components/navbar/Navbar"; // Assuming Navbar is a separate component
 import { useRouter, useSearchParams } from "next/navigation";
 import Grid from "../components/grid/Grid";
@@ -56,7 +56,7 @@ const Page = () => {
       Topics: "topics",
     };
     const newSearch = optionToSearchMap[newOption];
-    setSearch(newSearch);
+    // setSearch(newSearch);
     router.push(`/search?search=${newSearch}`);
   };
 
@@ -66,57 +66,59 @@ const Page = () => {
   };
 
   return (
-    <div className="min-h-screen">
-      <div className="relative h-[350px]">
-        <Navbar styleProps={{ color: "white" }} />
-        <div
-          className="absolute top-0 left-0 w-full h-[350px]"
-          style={{
-            backgroundImage: `url(https://preview.redd.it/national-park-4k-3840x2160-by-a-i-v0-g4crddfnmt9a1.jpg?auto=webp&s=5e92a3bb0952435a962a9071dc0f6e4bcf95d996)`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            zIndex: -1,
-          }}
-        />
-        <div className="flex flex-col items-center pt-24 gap-4">
-          <p className="text-4xl md:text-6xl font-extrabold text-white">
-            {selectedOption ? `Search for ${selectedOption}` : "Search"}
-          </p>
-          <form className="flex flex-col items-center w-full mt-8 md:flex-row md:px-0 lg:px-16 px-4">
-            <input
-              placeholder="Search by Activity"
-              type="text"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              className="flex-grow w-full h-12 px-4 mb-3 text-white transition duration-200 border-2 border-transparent rounded appearance-none md:mr-2 md:mb-0 bg-[#389B87] focus:outline-none focus:shadow-outline"
-            />
-            <select
-              value={selectedOption}
-              onChange={handleSelectChange}
-              className="flex-grow w-full h-12 px-4 mb-3 text-white transition duration-200 border-2 border-transparent rounded appearance-none md:mr-2 md:mb-0 bg-[#389B87] focus:outline-none focus:shadow-outline"
-            >
-              <option value="">Select Category</option>
-              {Object.entries(searchOptions).map(([key, value]) => (
-                <option key={key} value={value}>
-                  {value}
-                </option>
-              ))}
-            </select>
-            <button
-              type="button"
-              className="inline-flex items-center justify-center w-full h-12 px-6 font-semibold tracking-wide text-[#389B87] transition duration-200 rounded shadow-md md:w-auto hover:text-[#389B87] bg-[#FEFCFB] focus:shadow-outline focus:outline-none"
-              onClick={handleSearchButtonClick}
-            >
-              Search
-            </button>
-          </form>
+    <Suspense>
+      <div className="min-h-screen">
+        <div className="relative h-[350px]">
+          <Navbar styleProps={{ color: "white" }} />
+          <div
+            className="absolute top-0 left-0 w-full h-[350px]"
+            style={{
+              backgroundImage: `url(https://preview.redd.it/national-park-4k-3840x2160-by-a-i-v0-g4crddfnmt9a1.jpg?auto=webp&s=5e92a3bb0952435a962a9071dc0f6e4bcf95d996)`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              zIndex: -1,
+            }}
+          />
+          <div className="flex flex-col items-center pt-24 gap-4">
+            <p className="text-4xl md:text-6xl font-extrabold text-white">
+              {selectedOption ? `Search for ${selectedOption}` : "Search"}
+            </p>
+            <form className="flex flex-col items-center w-full mt-8 md:flex-row md:px-0 lg:px-16 px-4">
+              <input
+                placeholder="Search by Activity"
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="flex-grow w-full h-12 px-4 mb-3 text-white transition duration-200 border-2 border-transparent rounded appearance-none md:mr-2 md:mb-0 bg-[#389B87] focus:outline-none focus:shadow-outline"
+              />
+              <select
+                value={selectedOption}
+                onChange={handleSelectChange}
+                className="flex-grow w-full h-12 px-4 mb-3 text-white transition duration-200 border-2 border-transparent rounded appearance-none md:mr-2 md:mb-0 bg-[#389B87] focus:outline-none focus:shadow-outline"
+              >
+                <option value="">Select Category</option>
+                {Object.entries(searchOptions).map(([key, value]) => (
+                  <option key={key} value={value}>
+                    {value}
+                  </option>
+                ))}
+              </select>
+              <button
+                type="button"
+                className="inline-flex items-center justify-center w-full h-12 px-6 font-semibold tracking-wide text-[#389B87] transition duration-200 rounded shadow-md md:w-auto hover:text-[#389B87] bg-[#FEFCFB] focus:shadow-outline focus:outline-none"
+                onClick={handleSearchButtonClick}
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+        <div>
+          {/* Replace this with your search results component or logic */}
+          <Grid />
         </div>
       </div>
-      <div>
-        {/* Replace this with your search results component or logic */}
-        <Grid />
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
